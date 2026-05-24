26d373b0d143f1490d6822dc5219ace6a7dd46f6
Ruchita Gor <117519687+RuchitaGor@users.noreply.github.com>
2026-05-24 22:38:34 +0530

    Add FirstServlet to handle GET requests

---
+++ First-Servlet-Program/FirstServlet.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Hello First Servlet</h1>");
+    }
+}
